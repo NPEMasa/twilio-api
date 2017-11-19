4ae7722c29f6a2dd861e5290ebf847bf2b877063
Masanobu Miyagi <npemasa@outlook.jp>
2017-11-20 08:46:03 +0900

    twilio-notify with PHP

---
+++ twilio-nf.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'path/to/your/autoload.php'; 
+use Twilio\Rest\Client;
+
+if(!empty($_GET['msg'])){
+  $msg = "This message is Twilio test msg's.";
+}else{
+  $msg = htmlspecialchars($_GET['msg']);
+}
+
+$sid = "<your twilio API sid>";
+$token = "<your twilio API token id>";
+$client = new Client($sid, $token);
+
+$client->messages
+    ->create(
+        "+<send to phone number>",
+        array(
+            "from" => "+<from twilio number>",
+            "body" => $msg,
+        )
+    );
+
+echo $client;
+
+?>
